970f7bc94d9f78842d94908b0e2ff16ddd446e15
Kmj130830 <sunae0328@gmail.com>
2026-08-30 15:10:40 +0900

    Add per-track singer and phonemizer controls

--- include/UI/TrackPanel.h
+++ include/UI/TrackPanel.h
@@ -1,4 +1,27 @@
 #pragma once
+
 #include <QListWidget>
 #include "Core/Project.h"
-namespace myvocal { class TrackPanel : public QListWidget { Q_OBJECT public: explicit TrackPanel(Project*,QWidget*parent=nullptr); void refresh(); signals: void trackSelected(int); private: Project*m_project; }; }
+
+namespace myvocal {
+
+class SingerManager;
+
+class TrackPanel : public QListWidget {
+    Q_OBJECT
+public:
+    explicit TrackPanel(Project* project, SingerManager* singers, QWidget* parent = nullptr);
+
+    void setSingerManager(SingerManager* singers);
+    void refresh();
+
+signals:
+    void trackSelected(int index);
+    void trackSettingsChanged(int index);
+
+private:
+    Project* m_project;
+    SingerManager* m_singers;
+};
+
+}
