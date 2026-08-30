2f36704530555f859e62567930fbcc48236c8d19
Kmj130830 <sunae0328@gmail.com>
2026-08-30 15:11:54 +0900

    Expand PianoRoll editor interaction model

--- include/Editor/PianoRollEditor.h
+++ include/Editor/PianoRollEditor.h
@@ -1,10 +1,87 @@
 #pragma once
+
 #include <QAbstractScrollArea>
-#include <QVector>
 #include <QPoint>
 #include <QRect>
+#include <QVector>
 #include "Core/Project.h"
+
 namespace myvocal {
-enum class EditTool { Select, Pen, PenPlus, Eraser, Knife, Pitch, PitchLine, Vibrato, Zoom, Pan };
-class PianoRollEditor : public QAbstractScrollArea { Q_OBJECT public: explicit PianoRollEditor(Project* project,QWidget* parent=nullptr); void setActiveTrack(int index); void setTool(EditTool tool); EditTool tool()const noexcept; void setPlayheadTick(qint64 tick); qint64 playheadTick()const noexcept; void setSnapEnabled(bool enabled); bool snapEnabled()const noexcept; void setShowGrid(bool v); signals: void documentChanged(); void noteDoubleClicked(qint64 id); void requestPlaybackTick(qint64 tick); protected: void paintEvent(QPaintEvent*) override; void mousePressEvent(QMouseEvent*) override; void mouseMoveEvent(QMouseEvent*) override; void mouseReleaseEvent(QMouseEvent*) override; void wheelEvent(QWheelEvent*) override; void keyPressEvent(QKeyEvent*) override; private: qint64 snapTick(qint64 tick)const; qint64 tickAtX(int x)const; int midiAtY(int y)const; QRect noteRect(const Note&n)const; Note* noteAt(const QPoint&pos); void ensureVisibleTick(qint64 tick); void createNote(const QPoint&pos); void deleteNoteAt(const QPoint&pos); void moveSelectedBy(const QPoint&delta); void resizeSelectedBy(int dx); void invalidate(); Project* m_project; int m_activeTrack{0}; EditTool m_tool{EditTool::Select}; qint64 m_playhead{0}; bool m_snap{true}; bool m_showGrid{true}; double m_pxPerBeat{110.0}; int m_rowHeight{22}; int m_topMidi{84}; bool m_dragging{false}; bool m_resizing{false}; bool m_panning{false}; QPoint m_lastPos; qint64 m_dragStartTick{0}; int m_dragStartPitch{60}; QVector<qint64> m_dragIds; };
+
+enum class EditTool {
+    Select, Pen, PenPlus, Eraser, Knife, Pitch, PitchLine, Vibrato, Zoom, Pan
+};
+
+class PianoRollEditor : public QAbstractScrollArea {
+    Q_OBJECT
+public:
+    explicit PianoRollEditor(Project* project, QWidget* parent = nullptr);
+
+    void setActiveTrack(int index);
+    int activeTrack() const noexcept;
+    void setTool(EditTool tool);
+    EditTool tool() const noexcept;
+    void setPlayheadTick(qint64 tick);
+    qint64 playheadTick() const noexcept;
+    void setSnapEnabled(bool enabled);
+    bool snapEnabled() const noexcept;
+    void setShowGrid(bool value);
+    bool showGrid() const noexcept;
+    void setGridTicks(qint64 ticks);
+    qint64 gridTicks() const noexcept;
+    void setKeyboardPitch(int midi);
+
+signals:
+    void documentChanged();
+    void noteDoubleClicked(qint64 id);
+    void requestPlaybackTick(qint64 tick);
+    void keyboardPreviewRequested(int midi);
+    void verticalPitchChanged(int topMidi);
+
+protected:
+    void paintEvent(QPaintEvent*) override;
+    void resizeEvent(QResizeEvent*) override;
+    void mousePressEvent(QMouseEvent*) override;
+    void mouseMoveEvent(QMouseEvent*) override;
+    void mouseReleaseEvent(QMouseEvent*) override;
+    void wheelEvent(QWheelEvent*) override;
+    void keyPressEvent(QKeyEvent*) override;
+    void mouseDoubleClickEvent(QMouseEvent*) override;
+
+private:
+    qint64 snapTick(qint64 tick) const;
+    qint64 tickAtX(int x) const;
+    int midiAtY(int y) const;
+    QRect noteRect(const Note& note) const;
+    Note* noteAt(const QPoint& pos);
+    bool isResizeHandle(const Note& note, const QPoint& pos) const;
+    void ensureVisibleTick(qint64 tick);
+    void createNote(const QPoint& pos);
+    void deleteNoteAt(const QPoint& pos);
+    void splitNoteAt(const QPoint& pos);
+    void invalidate();
+    void updateScrollRanges();
+
+    Project* m_project;
+    int m_activeTrack{0};
+    EditTool m_tool{EditTool::Pen};
+    qint64 m_playhead{0};
+    bool m_snap{true};
+    bool m_showGrid{true};
+    qint64 m_gridTicks{120};
+    double m_pxPerBeat{110.0};
+    int m_rowHeight{22};
+    int m_topMidi{108};
+    bool m_dragging{false};
+    bool m_resizing{false};
+    bool m_panning{false};
+    bool m_zooming{false};
+    QPoint m_lastPos;
+    qint64 m_dragStartTick{0};
+    int m_dragStartPitch{60};
+    QVector<qint64> m_dragIds;
+    qint64 m_resizeId{-1};
+    qint64 m_resizeOriginalEnd{0};
+};
+
 }
