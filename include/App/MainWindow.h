486489295244d21d5b331728d0f7e98ef020e174
Kmj130830 <sunae0328@gmail.com>
2026-08-30 15:12:58 +0900

    Connect MainWindow to real singer/editor workflow

--- include/App/MainWindow.h
+++ include/App/MainWindow.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <QMainWindow>
 #include <memory>
 #include "Core/Project.h"
@@ -7,5 +8,61 @@
 #include "Renderer/Renderer.h"
 #include "Editor/PianoRollEditor.h"
 #include "Undo/UndoManager.h"
-namespace myvocal { class TrackPanel; class MainToolBar; class ParameterPanel; class TransportBar; class MainWindow final: public QMainWindow { Q_OBJECT public: explicit MainWindow(QWidget*parent=nullptr); private slots: void newProject(); void openProject(); void saveProject(); void saveProjectAs(); void importMidi(); void exportWav(); void addTrack(); void removeTrack(); void selectTrack(int); void setTool(EditTool); void togglePlay(); void stopPlayback(); void renderProject(); void renderSelected(); void showPreferences(); void showDiagnostics(); private: void buildUi(); void buildMenus(); void connectUi(); void setProject(std::unique_ptr<Project>); void updateTitle(); QString defaultProjectPath()const; std::unique_ptr<Project> m_project; AudioEngine m_audio; SingerManager m_singers; Renderer m_renderer; UndoManager m_undo; PianoRollEditor*m_editor{nullptr}; TrackPanel*m_trackPanel{nullptr}; MainToolBar*m_toolbar{nullptr}; ParameterPanel*m_params{nullptr}; TransportBar*m_transport{nullptr}; QString m_resampler; };
+
+namespace myvocal {
+
+class TrackPanel;
+class MainToolBar;
+class ParameterPanel;
+class TransportBar;
+class PianoKeyboard;
+
+class MainWindow final : public QMainWindow {
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+
+private slots:
+    void newProject();
+    void openProject();
+    void saveProject();
+    void saveProjectAs();
+    void importMidi();
+    void exportWav();
+    void addTrack();
+    void removeTrack();
+    void selectTrack(int index);
+    void setTool(EditTool tool);
+    void togglePlay();
+    void stopPlayback();
+    void renderProject();
+    void renderSelected();
+    void showPreferences();
+    void showDiagnostics();
+    void rescanVoiceBanks();
+
+private:
+    void buildUi();
+    void buildMenus();
+    void connectUi();
+    void setProject(std::unique_ptr<Project> project);
+    void applyDefaults(Project& project);
+    void refreshVoiceBanks();
+    void updateTitle();
+    QString defaultProjectPath() const;
+
+    std::unique_ptr<Project> m_project;
+    AudioEngine m_audio;
+    SingerManager m_singers;
+    Renderer m_renderer;
+    UndoManager m_undo;
+    PianoRollEditor* m_editor{nullptr};
+    PianoKeyboard* m_keyboard{nullptr};
+    TrackPanel* m_trackPanel{nullptr};
+    MainToolBar* m_toolbar{nullptr};
+    ParameterPanel* m_params{nullptr};
+    TransportBar* m_transport{nullptr};
+    QString m_resampler;
+};
+
 }
