85875198dcb302d504447a3c538f001170f3238d
Kmj130830 <sunae0328@gmail.com>
2026-08-30 15:11:01 +0900

    Expand preferences model for voicebanks and editor defaults

--- include/UI/PreferencesDialog.h
+++ include/UI/PreferencesDialog.h
@@ -1,4 +1,31 @@
 #pragma once
+
 #include <QDialog>
 #include <QString>
-namespace myvocal { class PreferencesDialog : public QDialog { Q_OBJECT public: explicit PreferencesDialog(QString*resampler,QWidget*parent=nullptr); private: QString*m_resampler; }; }
+
+namespace myvocal {
+
+class PreferencesDialog : public QDialog {
+    Q_OBJECT
+public:
+    explicit PreferencesDialog(QString* resampler, QWidget* parent = nullptr);
+
+    QString voiceBanksPath() const;
+    QString defaultSinger() const;
+    QString defaultPhonemizer() const;
+    double defaultBpm() const;
+    int defaultSnap() const;
+    int defaultGrid() const;
+
+private:
+    QString* m_resampler;
+    class QLineEdit* m_resamplerEdit{nullptr};
+    class QLineEdit* m_voiceBanksEdit{nullptr};
+    class QComboBox* m_singerCombo{nullptr};
+    class QComboBox* m_phonemizerCombo{nullptr};
+    class QDoubleSpinBox* m_bpmSpin{nullptr};
+    class QSpinBox* m_snapSpin{nullptr};
+    class QSpinBox* m_gridSpin{nullptr};
+};
+
+}
