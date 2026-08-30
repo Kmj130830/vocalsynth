486cd5824a3d809a424b420ff6bb1bfab01bcab8
Kmj130830 <sunae0328@gmail.com>
2026-08-30 16:43:11 +0900

    Implement grid resolution toolbar

--- src/UI/MainToolBar.cpp
+++ src/UI/MainToolBar.cpp
@@ -1,3 +1,73 @@
 #include "UI/MainToolBar.h"
+
 #include <QActionGroup>
-namespace myvocal { MainToolBar::MainToolBar(QWidget*p):QToolBar(p){setIconSize(QSize(18,18));setMovable(false);m_group=new QActionGroup(this);m_group->setExclusive(true);const QList<QPair<QString,EditTool>> tools={{"Select",EditTool::Select},{"Pen",EditTool::Pen},{"Pen+",EditTool::PenPlus},{"Eraser",EditTool::Eraser},{"Knife",EditTool::Knife},{"Pitch",EditTool::Pitch},{"Pitch Line",EditTool::PitchLine},{"Vibrato",EditTool::Vibrato},{"Zoom",EditTool::Zoom},{"Pan",EditTool::Pan}};for(const auto&it:tools){auto*a=addAction(it.first);a->setCheckable(true);a->setToolTip(it.first);a->setData(static_cast<int>(it.second));m_group->addAction(a);connect(a,&QAction::triggered,this,[this,a](){emit toolChanged(static_cast<EditTool>(a->data().toInt()));});if(it.second==EditTool::Select)a->setChecked(true);}addSeparator();auto*snap=addAction("Snap");snap->setCheckable(true);snap->setChecked(true);connect(snap,&QAction::toggled,this,&MainToolBar::snapToggled);auto*grid=addAction("Grid");grid->setCheckable(true);grid->setChecked(true);connect(grid,&QAction::toggled,this,&MainToolBar::gridToggled);}}
+#include <QComboBox>
+#include <QLabel>
+
+namespace myvocal {
+
+MainToolBar::MainToolBar(QWidget* parent)
+    : QToolBar(parent)
+{
+    setIconSize(QSize(18, 18));
+    setMovable(false);
+
+    m_group = new QActionGroup(this);
+    m_group->setExclusive(true);
+
+    const QList<QPair<QString, EditTool>> tools = {
+        {QStringLiteral("Select"), EditTool::Select},
+        {QStringLiteral("Pen"), EditTool::Pen},
+        {QStringLiteral("Pen+"), EditTool::PenPlus},
+        {QStringLiteral("Eraser"), EditTool::Eraser},
+        {QStringLiteral("Knife"), EditTool::Knife},
+        {QStringLiteral("Pitch"), EditTool::Pitch},
+        {QStringLiteral("Pitch Line"), EditTool::PitchLine},
+        {QStringLiteral("Vibrato"), EditTool::Vibrato},
+        {QStringLiteral("Zoom"), EditTool::Zoom},
+        {QStringLiteral("Pan"), EditTool::Pan}
+    };
+
+    for (const auto& item : tools) {
+        QAction* action = addAction(item.first);
+        action->setCheckable(true);
+        action->setToolTip(item.first);
+        action->setData(static_cast<int>(item.second));
+        m_group->addAction(action);
+        connect(action, &QAction::triggered, this, [this, action] {
+            emit toolChanged(static_cast<EditTool>(action->data().toInt()));
+        });
+        if (item.second == EditTool::Pen) {
+            action->setChecked(true);
+        }
+    }
+
+    addSeparator();
+
+    QAction* snap = addAction(QStringLiteral("Snap"));
+    snap->setCheckable(true);
+    snap->setChecked(true);
+    connect(snap, &QAction::toggled, this, &MainToolBar::snapToggled);
+
+    QAction* grid = addAction(QStringLiteral("Grid"));
+    grid->setCheckable(true);
+    grid->setChecked(true);
+    connect(grid, &QAction::toggled, this, &MainToolBar::gridToggled);
+
+    addWidget(new QLabel(QStringLiteral("Grid:"), this));
+    m_gridCombo = new QComboBox(this);
+    m_gridCombo->addItem(QStringLiteral("1/4"), 4);
+    m_gridCombo->addItem(QStringLiteral("1/8"), 8);
+    m_gridCombo->addItem(QStringLiteral("1/16"), 16);
+    m_gridCombo->addItem(QStringLiteral("1/32"), 32);
+    m_gridCombo->addItem(QStringLiteral("1/64"), 64);
+    m_gridCombo->setCurrentIndex(2);
+    m_gridCombo->setToolTip(QStringLiteral("Grid resolution"));
+    addWidget(m_gridCombo);
+
+    connect(m_gridCombo, &QComboBox::currentIndexChanged, this, [this](int index) {
+        emit gridResolutionChanged(m_gridCombo->itemData(index).toInt());
+    });
+}
+
+}
