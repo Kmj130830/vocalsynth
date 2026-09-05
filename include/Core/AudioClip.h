4a2ceb1f0f0f2e1d9edb77dae260f4e1f9d4b899
Kmj130830 <sunae0328@gmail.com>
2026-09-05 20:24:27 +0900

    Store real audio clip duration in project format

--- include/Core/AudioClip.h
+++ include/Core/AudioClip.h
@@ -9,6 +9,7 @@ struct AudioClip {
     QString path;
     qint64 startMs{0};
     qint64 offsetMs{0};
+    qint64 durationMs{0};
     double volume{1.0};
     bool muted{false};
 
@@ -17,6 +18,7 @@ struct AudioClip {
         o["path"] = path;
         o["startMs"] = QString::number(startMs);
         o["offsetMs"] = QString::number(offsetMs);
+        o["durationMs"] = QString::number(durationMs);
         o["volume"] = volume;
         o["muted"] = muted;
         return o;
@@ -27,6 +29,7 @@ struct AudioClip {
         clip.path = o["path"].toString();
         clip.startMs = o["startMs"].toString().toLongLong();
         clip.offsetMs = o["offsetMs"].toString().toLongLong();
+        clip.durationMs = o["durationMs"].toString().toLongLong();
         clip.volume = o["volume"].toDouble(1.0);
         clip.muted = o["muted"].toBool(false);
         return clip;
